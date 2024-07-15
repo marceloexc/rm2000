8292dc3563a95ed9e4c3c25282983fc08af8bad9
marceloexc <marceloexc@tutanota.de>
2024-07-15 14:16:35 -0500

    initial "hello world"

---
+++ replica/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  replica
+//
+//  Created by Marcelo Mendez on 7/13/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "heart.fill")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ replica/replicaApp.swift
@@ -0,0 +1,17 @@
+//
+//  replicaApp.swift
+//  replica
+//
+//  Created by Marcelo Mendez on 7/13/24.
+//
+
+import SwiftUI
+
+@main
+struct replicaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
