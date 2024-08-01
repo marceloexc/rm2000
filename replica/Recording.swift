2d48422d1fd6c27eed6eb305735bb78c453254d3
marceloexc <marceloexc@tutanota.de>
2024-08-01 16:06:35 -0500

    update Recording.swift

--- replica/Recording.swift
+++ replica/Recording.swift
@@ -11,52 +11,68 @@ import ScreenCaptureKit
 import CoreGraphics
 import VideoToolbox
 
-
 class RecordingManager: ObservableObject {
 	
 	private var stream: SCStream?
 	private var assetWriter: AVAssetWriter?
 	private var videoInput: AVAssetWriterInput?
+	private var streamOutput: SCStreamOutput?
 	
 	func startRecording(filename: String, directory: String) async throws {
 		
-		let fullFilename: String = directory + filename
-				
-		// Construct a full fileURL from the two strings
+		// construct a full fileURL from the two strings
 		let directoryURL = URL(fileURLWithPath: directory)
 		let fileURL = directoryURL.appendingPathComponent(filename)
-		// Should now look something like file:///Users/Johnny/Downloads/Now%20recording
-				
+		
+		// should now look something like file:///Users/Johnny/Downloads/Now%20recording
 		assetWriter = try AVAssetWriter(outputURL: fileURL, fileType: .mov)
 		print("Successfully created AVAssetWriter for \(fileURL)")
 		
-		videoInput = AVAssetWriterInput(mediaType: .video, outputSettings: nil)
+		let videoSettings: [String: Any] = [
+			AVVideoCodecKey: AVVideoCodecType.h264,
+			AVVideoWidthKey: 1920,
+			AVVideoHeightKey: 1080,
+			AVVideoCompressionPropertiesKey: [
+				AVVideoAverageBitRateKey: 6000000,
+				AVVideoProfileLevelKey: AVVideoProfileLevelH264HighAutoLevel
+			]
+		]
+		
+		videoInput = AVAssetWriterInput(mediaType: .video, outputSettings: videoSettings)
+		
+		guard assetWriter!.canAdd(videoInput!) else {
+			print("Cannot add video input to asset writer")
+			return
+		}
 		
 		assetWriter?.add(videoInput!)
+		
 		assetWriter?.startWriting()
 		
-		// Start session
-		
+		// start session
 		let now = CMClockGetTime(CMClockGetHostTimeClock())
 		assetWriter?.startSession(atSourceTime: now)
 		
-		// Get everything ready for SCStream
-		
-		// Set up configuration for the stream
+		// set up configuration for the stream
 		let configuration = SCStreamConfiguration()
 		configuration.width = 1920
 		configuration.height = 1080
 		configuration.minimumFrameInterval = CMTime(value: 1, timescale: 30) // 30fps for now
 		configuration.pixelFormat = kCVPixelFormatType_32BGRA
 		
-		// Getting the main display
+		// getting the main display
 		
-		let availableContent = try await SCShareableContent.excludingDesktopWindows(false, onScreenWindowsOnly: true)
+		// sometimes, this will never execute nor return an error code or anything.
+		// to fix this, i have to kill `replayd` from activity monitor
+		// and then on the next run it will start working.
+		// i have no idea why this happens
 		
-		// Still learning Swift: gets mad if I don't wrap it like this. Safety is key in this language
-		guard let display = availableContent.displays.first else {
-			throw NSError(domain:"RecordingError", code: 1, userInfo: [NSLocalizedDescriptionKey: "No displays available"])
+		let availableContent = try await SCShareableContent.current
+		guard let display = availableContent.displays.first(where: { $0.displayID == CGMainDisplayID() }) else {
+			throw NSError(domain:"RecordingError", code: 1, userInfo: [NSLocalizedDescriptionKey: "Can't find display with ID \(CGMainDisplayID() ) in sharable content"])
 		}
+		
+		print("Using \(display.description) as the main display")
 		
 		let filter = SCContentFilter(display: display, excludingWindows: [])
 		
@@ -64,10 +80,9 @@ class RecordingManager: ObservableObject {
 		
 		stream = SCStream(filter: filter, configuration: configuration, delegate: streamDelegate)
 		
-		// TODO: what does THIS DO?
 		try stream?.addStreamOutput(streamDelegate, type: .screen, sampleHandlerQueue: DispatchQueue.global(qos: .userInitiated))
 		
-		try await stream!.startCapture()
+		try await stream?.startCapture()
 		
 		print("Screen recording started!")
 	}
@@ -76,6 +91,7 @@ class RecordingManager: ObservableObject {
 		try await stream?.stopCapture()
 		stream = nil
 		videoInput?.markAsFinished()
+		assetWriter?.endSession(atSourceTime: <#T##CMTime#>)
 		await assetWriter?.finishWriting()
 		assetWriter = nil
 		videoInput = nil
@@ -86,15 +102,17 @@ class RecordingManager: ObservableObject {
 class ScreenRecorderDelegate: NSObject, SCStreamDelegate, SCStreamOutput {
 	var assetWriter: AVAssetWriter
 	var videoInput: AVAssetWriterInput
+//	var lastSampleBuffer: CMSampleBuffer?
 	
 	init(assetWriter: AVAssetWriter, videoInput: AVAssetWriterInput) {
 		self.assetWriter = assetWriter
 		self.videoInput = videoInput
 	}
 	
-	// What does THIS do?
 	func stream(_ stream: SCStream, didOutputSampleBuffer sampleBuffer: CMSampleBuffer, of type: SCStreamOutputType) {
 		guard type == .screen, videoInput.isReadyForMoreMediaData else { return }
-		videoInput.append(sampleBuffer)
+		if !videoInput.append(sampleBuffer) {
+			print("Failed to append sample buffer")
+		}
 	}
 }
