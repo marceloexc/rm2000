ddb8acb720121e6b4e6447ca1ec6dd2054eb98f0
marceloexc <marceloexc@tutanota.de>
2025-03-11 01:34:07 -0500

    actually check if the directory is there! (temp)

--- rm2000/Models/NewRecording.swift
+++ rm2000/Models/NewRecording.swift
@@ -1,4 +1,5 @@
 import Foundation
+import OSLog
 
 struct NewRecording {
 	var id: UUID
@@ -6,6 +7,17 @@ struct NewRecording {
 	
 	// TODO - hardcoded file extension string
 	init() {
+		
+		// ensure directory exists
+		// TODO - terrible - maybe belongs in SampleStorage instead?
+		// (why are we still using workingdirectory? that thing needs to die...
+		if !(WorkingDirectory.applicationSupportPath().isDirectory) {
+			
+			let directory = WorkingDirectory.applicationSupportPath()
+			
+			try? FileManager.default.createDirectory(at: directory, withIntermediateDirectories: true)
+			Logger().info("Had to make a directory for the application support path at: \(directory)")
+		}
 		self.id = UUID()
 		self.fileURL = WorkingDirectory.applicationSupportPath()
 			.appendingPathComponent("\(id.uuidString).aac")
